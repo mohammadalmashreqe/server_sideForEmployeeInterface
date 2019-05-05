c94f03dc2cef314ee7b4bbe389e3721e2f2c630c
mohammadalmashreqe <mohammadalmashreqe@yahoo.com>
2019-05-05 16:33:28 +0300

    commirte

---
+++ Server/index.js
@@ -0,0 +1,81 @@
+let express = require('express')
+let app = express();
+const TicketsList = new Array();
+TicketsList.push("A001");
+TicketsList.push("A002");
+let http = require('http');
+let server = http.Server(app);
+
+let socketIO = require('socket.io');
+let io = socketIO(server);
+
+/**
+ *Port Number
+ *
+ */
+const port = process.env.PORT || 3000;
+/**
+ *event fires when user connect 
+ *
+ */
+io.on('connection', (socket) => {
+    try {
+    console.log('Emplyee connected');
+    io.emit("UpadteList", TicketsList);
+    socket.on('Serve-Tickets', () => {
+
+        TicketsList.splice(TicketsList.length-1,1);
+        
+        //here i want to tell rabbit q about what happens .
+
+        updateTickitsList();
+    });
+    }
+    catch(err)
+    {
+        console.log(err.message);
+    }
+
+});
+
+io.on('Serve-Tickets', () => {
+
+    TicketsList.splice(TicketsList.length()-1,1);
+    
+    //here i want to tell rabbit q about what happens .
+
+    updateTickitsList();
+});
+/**
+ *listen to the request  
+ *
+ */
+server.listen(port, () => {
+    try {
+    console.log(`started on port: ${port}`);
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+});
+
+
+
+
+/**
+ *update tickets List and notify all connected user 
+ *
+ */
+function updateTickitsList() {
+    try {
+    //notify all about update 
+    io.emit("UpadteList", TicketsList);
+    //to do :
+    // sent to rabit  to store ticket in MQ
+    }
+    catch (err)
+    {
+        console.log(err);
+    }
+}
