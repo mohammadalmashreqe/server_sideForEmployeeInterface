489d9722a39e67c97ad1015d234a30b0cd97a142
mohammadalmashreqe <mohammadalmashreqe@yahoo.com>
2019-05-07 16:12:47 +0300

    fix

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,45 @@
 let express = require('express')
 let app = express();
 const TicketsList = new Array();
-TicketsList.push("A001");
-TicketsList.push("A002");
-TicketsList.push("A003");
-TicketsList.push("A004");
+
 let http = require('http');
 let server = http.Server(app);
-
+var amqp = require('amqplib/callback_api');
 let socketIO = require('socket.io');
 let io = socketIO(server);
+
+
+/**
+* API to handel serving 
+*
+*/
+
+
+app.get("/serveTicket", (req, res) => {
+    try {
+      
+     //   io.emit("UpadteList", TicketsList);
+        //save tickits in Rabbit Message Queue 
+        //produceTickitsToMQ(TicketsList[TicketsList.length - 1]);
+        console.log("serve from server side");
+res.send("HI");
+        }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+
+
+
+
+
 
 /**
  *Port Number
  *
  */
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8124;
 /**
  *event fires when user connect 
  *
@@ -23,15 +47,44 @@ const port = process.env.PORT || 3000;
 io.on('connection', (socket) => {
     try {
     console.log('Emplyee connected');
-    io.emit("UpadteList", TicketsList);
-    socket.on('Serve-Tickets', () => {
-
-        TicketsList.splice(0,1);
-        
-        //here i want to tell rabbit q about what happens .
-
-        updateTickitsList();
-    });
+      
+    amqp.connect('amqp://localhost', function(error0, connection) {
+        if (error0) {
+          throw error0;
+        }
+        connection.createChannel(function(error1, channel) {
+          if (error1) {
+            throw error1;
+          }
+          var exchange = 'Tickets';
+      
+          channel.assertExchange(exchange, 'fanout', {
+            durable: false
+          });
+      
+          channel.assertQueue('', {
+            exclusive: true
+          }, function(error2, q) {
+            if (error2) {
+              throw error2;
+            }
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+            channel.bindQueue(q.queue, exchange, '');
+         
+      
+            channel.consume(q.queue, function(msg) {
+              if(msg.content) {
+                  console.log(" [x] %s", msg.content.toString());
+                  TicketsList.push(msg);
+                }
+            }, {
+              noAck: true
+            });
+          });
+        });
+      });
+   io.emit("UpadteList", TicketsList);
+ 
     }
     catch(err)
     {
@@ -40,20 +93,7 @@ io.on('connection', (socket) => {
 
 });
 
-io.on('Serve-Tickets', () => {
-try {
-    TicketsList.splice(0,1);
 
-    
-    //here i want to tell rabbit q about what happens .
-
-    updateTickitsList();
-}
-catch(err)
-{
-    console.log(err);
-}
-});
 /**
  *listen to the request  
  *
@@ -74,7 +114,7 @@ server.listen(port, () => {
 /**
  *update tickets List and notify all connected user 
  *
- */
+ 
 function updateTickitsList() {
     try {
     //notify all about update 
@@ -86,4 +126,4 @@ function updateTickitsList() {
     {
         console.log(err);
     }
-}
+}*/
