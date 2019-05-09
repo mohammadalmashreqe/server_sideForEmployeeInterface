481049faa5e904be413abcbddc15922892326a42
mohammadalmashreqe <mohammadalmashreqe@yahoo.com>
2019-05-09 11:06:36 +0300

    message transfer from producer to consumer

--- Server/index.js
+++ Server/index.js
@@ -1,128 +1,118 @@
 let express = require('express')
 let app = express();
-const TicketsList = new Array();
+var TicketsList = new Array();
 
+const ticketsnumber = "A00";
 let http = require('http');
 let server = http.Server(app);
 var amqp = require('amqplib/callback_api');
+
 let socketIO = require('socket.io');
 let io = socketIO(server);
 
+var connection;
 
+var conn = require('amqplib/callback_api');
+conn.connect('amqp://localhost', function (err, conn) {
+  if (err != null) bail(err);
+  connection=conn;
+  consumer(conn);
+});
 /**
-* API to handel serving 
+* API to handel customet request 
 *
 */
 
-
 app.get("/serveTicket", (req, res) => {
-    try {
-      
-        
-
-      amqp.connect('amqp://localhost', function(error0, connection) {
-        if (error0) {
-          throw error0;
-        }
-        connection.createChannel(function(error1, channel) {
-          if (error1) {
-            throw error1;
-          }
-          var exchange = 'Tickets';
-      
-          channel.assertExchange(exchange, 'fanout', {
-            durable: false
-          });
-      
-          channel.assertQueue('', {
-            exclusive: true
-          }, function(error2, q) {
-            if (error2) {
-              throw error2;
-            }
-            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-            channel.bindQueue(q.queue, exchange, '');
-         
-      
-            channel.consume(q.queue, function(msg) {
-              if(msg.content) {
-                  console.log(" [x] %s", msg.content.toString());
-                  TicketsList.push(msg);
-                }
-            }, {
-              noAck: true
-            });
-          });
-        });
-      });
-
-      io.emit("UpadteList", TicketsList); }
-    catch (err) {
-        console.log(err);
+  try {
+    TicketsList=TicketsList.splice(0,1);
+    if (connection) {
+      publisher(connection);
     }
+    else
+      console.log("error no conn");
+
+  }
+  catch (err) {
+    console.log(err);
+  }
 });
-
-
-
-
-
-
-
 /**
  *Port Number
  *
  */
-const port = process.env.PORT || 8124;
+const port = process.env.PORT || 4000;
+
 /**
  *event fires when user connect 
  *
  */
+
 io.on('connection', (socket) => {
-    try {
-    console.log('Emplyee connected');
-      
-   
-   io.emit("UpadteList", TicketsList);
- 
-    }
-    catch(err)
-    {
-        console.log(err.message);
-    }
+  try {
+    console.log('user connected');
+
+    io.emit("UpadteList", TicketsList);
+
+  }
+  catch (err) {
+    console.log(err.message);
+  }
 
 });
-
 
 /**
  *listen to the request  
  *
  */
 server.listen(port, () => {
-    try {
+  try {
     console.log(`started on port: ${port}`);
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
+  }
+  catch (err) {
+    console.log(err);
+  }
 });
 
+function bail(err) {
+  console.error(err);
+  process.exit(1);
+}
 
-
-
-/**
- *update tickets List and notify all connected user 
- *
- 
-function updateTickitsList() {
-    try {
-    //notify all about update 
-    io.emit("UpadteList", TicketsList);
-    //to do :
-    // sent to rabit  to store ticket in MQ
+// Publisher
+function publisher(conn) {
+  try{
+    conn.createChannel(on_open);
+    function on_open(err, ch) {
+      if (err != null) bail(err);
+      ch.assertQueue(q);
+      ch.sendToQueue(q, Buffer.from(JSON.stringify(TicketsList)));
+      console.log("raye7 ")
     }
-    catch (err)
-    {
-        console.log(err);
-    }
-}*/
+  }catch (err) {
+    console.log(err);
+  }
+  
+}
+var q = 'tasks';
+
+// Consumer
+function consumer(conn) {
+  
+  var ok = conn.createChannel(on_open);
+  function on_open(err, ch) {
+    if (err != null) bail(err);
+    ch.assertQueue(q);
+    ch.consume(q, function (pTicketsList) {
+      console.log("Consume ^^")
+      if (pTicketsList !== null) {
+        console.log(pTicketsList.content.toString('utf8'));
+        TicketsList=JSON.parse(pTicketsList.content.toString('utf8'));
+        //io.emit("onListUpdate", pTicketsList);
+        ch.ack(pTicketsList);
+      }
+    });
+  }
+}
+
+
